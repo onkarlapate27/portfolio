e2630fe2e138d318b35b466620568cd03ad4b431
Onkar Lapate <85246478+onkarlapate27@users.noreply.github.com>
2022-05-19 12:09:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var formSubmitted = document.getElementById('submit-button');
+
+function printSubmit(){
+    alert('The Form is Submitted!');
+}
+
+formSubmitted.addEventListener('click', printSubmit);
